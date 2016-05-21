07718fd5c9706eeb24f20e7ce1df6a251fa56d0b
Brian Roper <broper138@gmail.com>
2016-05-21 12:31:29 -0400

    0.4
    feat: Nav drawer
    
    -added nav drawer to toolbar

--- app/src/main/java/com/brianroper/tattome/ListActivity.java
+++ app/src/main/java/com/brianroper/tattome/ListActivity.java
@@ -1,13 +1,20 @@
 package com.brianroper.tattome;
 
+import android.app.Fragment;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.firebase.client.Firebase;
@@ -16,12 +23,28 @@ import java.io.ByteArrayOutputStream;
 
 public class ListActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView)findViewById(R.id.nav_view);
+
+        setUpDrawerContent(mNavigationView);
+
+        mActionBarDrawerToggle= setupDrawerToggle();
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+
+        //TODO: drawer functionality
+
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle(getApplicationContext()
                         .getResources()
                         .getString(R.string.app_title));
@@ -30,4 +53,68 @@ public class ListActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case android.R.id.home:
+                mDrawerLayout.isDrawerOpen(GravityCompat.START);
+                return true;
+
+        }
+
+        if(mActionBarDrawerToggle.onOptionsItemSelected(item)){
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setUpDrawerContent(NavigationView navigationView){
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem){
+
+        Fragment fragment = null;
+
+        Class fragmentClass;
+
+        switch(menuItem.getItemId()){
+
+            case R.id.nav_first_item:
+
+
+        }
+    }
+
+    public ActionBarDrawerToggle setupDrawerToggle(){
+            return new ActionBarDrawerToggle(this,
+                    mDrawerLayout,
+                    mToolbar,
+                    R.string.drawer_open,
+                    R.string.drawer_closed);
+
+    }
 }
